a3ffdf9a48efd9bbd26be954e14a7802418ef54a
cscpoly03 <fake@mailinator.com>
2019-08-13 16:23:24 -0700

    Migrated from Nashorn to GraalJS to support JDK8

--- src/main/java/com/jarslab/maven/babel/plugin/BabelTranspiler.java
+++ src/main/java/com/jarslab/maven/babel/plugin/BabelTranspiler.java
@@ -1,16 +1,11 @@
 package com.jarslab.maven.babel.plugin;
 
 import org.apache.maven.plugin.logging.Log;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import javax.script.SimpleBindings;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UncheckedIOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,7 +14,7 @@ import static java.util.Objects.requireNonNull;
 
 class BabelTranspiler
 {
-    private static final String JAVASCRIPT_MIME_TYPE = "text/javascript";
+    private static final String JAVASCRIPT_LANGUAGE = "js";
     private static final String INPUT_VARIABLE = "input";
     private static final String BABEL_EXECUTE = "Babel.transform(%s, {presets: [%s]}).code";
 
@@ -51,27 +46,23 @@ class BabelTranspiler
     void execute()
     {
         try {
-            final InputStreamReader fileReader = new InputStreamReader(
-                    new FileInputStream(babelSource), charset);
-            final ScriptEngine engine = new ScriptEngineManager(null)
-                    .getEngineByMimeType(JAVASCRIPT_MIME_TYPE);
-            final SimpleBindings simpleBindings = new SimpleBindings();
-            engine.eval(fileReader, simpleBindings);
-            final String source = new String(Files.readAllBytes(sourceFilePath), charset);
-            if (verbose) {
-                log.debug(String.format("%s source:\n%s", sourceFilePath, source));
+            final Context.Builder contextBuilder = Context.newBuilder(JAVASCRIPT_LANGUAGE);
+            try (final Context context = contextBuilder.build()) {
+                final Value bindings = context.getBindings(JAVASCRIPT_LANGUAGE);
+                context.eval(Source.newBuilder(JAVASCRIPT_LANGUAGE, babelSource).build());
+                final String source = new String(Files.readAllBytes(sourceFilePath), charset);
+                if (verbose) {
+                    log.debug(String.format("%s source:\n%s", sourceFilePath, source));
+                }
+                bindings.putMember(INPUT_VARIABLE, source);
+                final String result = context.eval(JAVASCRIPT_LANGUAGE, String.format(BABEL_EXECUTE, INPUT_VARIABLE, presets)).asString();
+                if (verbose) {
+                    log.debug(String.format("%s result:\n%s", sourceFilePath, result));
+                }
+                targetFileWriter.writeTargetFile(sourceFilePath, result);
             }
-            simpleBindings.put(INPUT_VARIABLE, source);
-            final String result = (String) engine.eval(
-                    String.format(BABEL_EXECUTE, INPUT_VARIABLE, presets), simpleBindings);
-            if (verbose) {
-                log.debug(String.format("%s result:\n%s", sourceFilePath, result));
-            }
-            targetFileWriter.writeTargetFile(sourceFilePath, result);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
-        } catch (ScriptException e) {
-            throw new RuntimeException(e);
         }
     }
 
